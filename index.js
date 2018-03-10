8c045b5d88ac8b1fb891089b060a2cc27032a511
Patrick Laplante <laplante.patrick@gmail.com>
2018-03-10 16:34:27 -0500

    added some example on how to do concurential and sequential download using bluebird

--- index.js
+++ index.js
@@ -20,21 +20,63 @@ function getDataUri(url, callback) {
 }
 
 const urls = [
-  "https://s3.amazonaws.com/ebsi-images/mfp_89986a2e05b2ad96122fb8bbf04ac71emm025090.jpg",
-  "https://s3.amazonaws.com/ebsi-images/mfp_89986a2e05b2ad96122fb8bbf04ac71emm025090.jpg",
-  "https://s3.amazonaws.com/ebsi-images/mfp_89986a2e05b2ad96122fb8bbf04ac71emm025090.jpg",
-  "https://s3.amazonaws.com/ebsi-images/mfp_89986a2e05b2ad96122fb8bbf04ac71emm025090.jpg",
-  "https://s3.amazonaws.com/ebsi-images/mfp_89986a2e05b2ad96122fb8bbf04ac71emm025090.jpg"
+  "https://s3.amazonaws.com/ebsi-files-new/000070dbcd51357fab7a3bc98f3ee413_mm032554.jpg",
+  "https://s3.amazonaws.com/ebsi-files-new/000070dbcd51357fab7a3bc98f3ee413_mm032554.jpg",
+  "https://s3.amazonaws.com/ebsi-files-new/000070dbcd51357fab7a3bc98f3ee413_mm032554.jpg",
+  "https://s3.amazonaws.com/ebsi-files-new/000070dbcd51357fab7a3bc98f3ee413_mm032554.jpg",
+  "https://s3.amazonaws.com/ebsi-files-new/000070dbcd51357fab7a3bc98f3ee413_mm032554.jpg",
+  "https://s3.amazonaws.com/ebsi-files-new/000070dbcd51357fab7a3bc98f3ee413_mm032554.jpg"
 ];
+
+const downloadInSeries = async urls => {
+  return await Promise.mapSeries(urls, async url => {
+    const data = await getDataUri(url);
+    return data;
+  });
+}
+
+async function downloadSingle(url) {
+  const data = await getDataUri(url);
+  return data;
+}
+
+// const downloadSingleSimpler = async url => await getDataUri(url);
+
+const wait = tout => new Promise((res, rej) => setTimeout(() => res(), tout));
+
+const downloadAllInParallel = async (urls, count = Infinity) => {
+  return await Promise.map(urls, async url => {
+    const data = await getDataUri(url);
+    return data;
+  }, {concurrency: count});
+}
 
 (async () => {
   const container = document.getElementById('container');
   container.innerHTML = "";
-  await Promise.mapSeries(urls, async url => {
-    const data = await getDataUri(url);
-    const img = document.createElement("img");
-    img.src = data;
-    container.appendChild(img);
-    console.log(data);
-  });
+
+  console.time('sequentially')
+  const resSequentially = await downloadAllInParallel(urls, 1);
+  console.timeEnd('sequentially')
+
+  console.time('chunkof8')
+  const resInChunkOf8 = await downloadAllInParallel(urls, 8);
+  console.timeEnd('chunkof8')
+  
+  console.time('allatonce')
+  const resAllAtOnce = await downloadAllInParallel(urls);
+  console.timeEnd('allatonce')
+  
+  
+  const oneImage = await downloadSingle(urls[0]);
+
+
+  // // Multiples in series
+  // await Promise.mapSeries(urls, async url => {
+  //   const data = await getDataUri(url);
+  //   const img = document.createElement("img");
+  //   img.src = data;
+  //   container.appendChild(img);
+  //   console.log(data);
+  // });
 })();
